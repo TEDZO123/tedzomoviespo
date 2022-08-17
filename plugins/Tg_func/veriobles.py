825c5e10e8f9c43dc163cb29790e94fa0b13c628
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-08-17 09:55:14 +0530

    Update veriobles.py

--- plugins/Tg_func/veriobles.py
+++ plugins/Tg_func/veriobles.py
@@ -5,23 +5,37 @@ from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from buttons import MENU_BUTTON
 
-@bot.on_message(filters.regex("@sithijatd"))
-async def sithijatdmsg(_,message):
-    m = message
-      await m.edit("||っ ͡❛ ͜ʖ ͡❛|っ🧠")
-      await message.edit("| |っ ͡❛ ͜ʖ ͡❛|っ🧠")
-      await m.edit("|  |っ ͡❛ ͜ʖ ͡❛|っ🧠")
-      await m.edit("|   |っ ͡❛ ͜ʖ ͡❛|っ🧠")
-      await m.edit("|    |っ ͡❛ ͜ʖ ͡❛|っ🧠")
-      await m.edit("|     |っ ͡❛ ͜ʖ ͡❛|っ🧠")
-      await m.edit("|      |っ ͡❛ ͜ʖ ͡❛|っ🧠")
-      await m.edit("|       |っ ͡❛ ͜ʖ ͡❛|っ🧠") 
-      await m.edit("|        |っ ͡❛ ͜ʖ ͡❛|っ🧠\n                  🗑")
-      await m.edit("|        |っ ͡❛ ͜ʖ ͡❛|っ  \n                  🗑")
-      await m.edit("|        |っ ͡❛ ͜ʖ ͡❛|っ  \n                  💩")
+@bot.on_message(filters.regex("@tedzo"))
+async def bak(bot, msg):
+    await msg.reply(text="<b>Enter Your FeedBack OR Request, I Will Give It To My Boss</b>")
+        await msg.message.edit("|( ͡❛ ͜ʖ ͡❛)")
+        await msg.message.edit("||っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await msg.message.edit("| |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await msg.message.edit("|  |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await msg.message.edit("|   |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await msg.message.edit("|    |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await msg.message.edit("|     |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await msg.message.edit("|      |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await msg.message.edit("|       |っ ͡❛ ͜ʖ ͡❛|っ🧠") 
+        await msg.message.edit("|        |っ ͡❛ ͜ʖ ͡❛|っ🧠\n                  🗑")
+        await msg.message.edit("|        |っ ͡❛ ͜ʖ ͡❛|っ  \n                  🗑")
+        await msg.message.edit("|        |っ ͡❛ ͜ʖ ͡❛|っ  \n                  💩")
         await message.reply_text(
-        text="hi bro",
-        reply_markup=MENU_BUTTON,
+        text="Hᴇʀᴇ Is Yᴏᴜ'ʀᴇ Mᴇɴᴜ",
+        reply_markup=InlineKeyboardMarkup(MENU_BUTTON)
         disable_web_page_preview=True
 )
-     await m.delete()
+    # await m.delete()
+@bot.on_message(filters.regex("REQUEST/FEEDBACK"))
+async def bak(bot, msg):
+    await msg.reply(text="<b>Enter Your FeedBack OR Request, I Will Give It To My Boss</b>")
+        await msg.message.edit("○○○○○")
+        await msg.message.edit("●○○○○")
+        await msg.message.edit("●●○○○")
+        await msg.message.edit("●●●○○")
+        await msg.message.edit("●●●●○")
+        await msg.message.edit("●●●●●")
+        await msg.message.edit(
+            text="Hᴇʀᴇ Is Yᴏᴜ'ʀᴇ Mᴇɴᴜ",
+            reply_markup=InlineKeyboardMarkup(MENU_BUTTON)
+        )
