fd9132046d1fc732223a40bb0f37c9c31752b4cf
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-08-17 10:11:24 +0530

    Update veriobles.py

--- plugins/Tg_func/veriobles.py
+++ plugins/Tg_func/veriobles.py
@@ -3,18 +3,58 @@ from bot import bot
 from pyrogram import filters, idle
 from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from buttons import MENU_BUTTON
+from info import GITHUB_TEXT, SITHIJATD_BUTTONS
 
-# await m.delete()
 @bot.on_message(filters.regex("@tedzo01"))
 async def bak(bot, msg):
     await msg.reply(text="<b>Enter Your FeedBack OR Request, I Will Give It To My Boss</b>")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("|( ͡❛ ͜ʖ ͡❛)")
+        await asyncio.sleep(0.2)
+        await asyncio.sleep(0.2)
+        await msg.message.edit("||っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("| |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("|  |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("|   |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("|    |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("|     |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("|      |っ ͡❛ ͜ʖ ͡❛|っ🧠")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("|       |っ ͡❛ ͜ʖ ͡❛|っ🧠") 
+        await msg.message.edit("|        |っ ͡❛ ͜ʖ ͡❛|っ🧠\n                  🗑")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("|        |っ ͡❛ ͜ʖ ͡❛|っ  \n                  🗑")
+        await asyncio.sleep(0.2)
+        await msg.message.edit("|        |っ ͡❛ ͜ʖ ͡❛|っ  \n                  💩")
+        await asyncio.sleep(0.2)
+       await message.reply_text(
+        text="Hᴇʀᴇ Is Yᴏᴜ'ʀᴇ Mᴇɴᴜ",
+        reply_markup=InlineKeyboardMarkup(MENU_BUTTON)
+        disable_web_page_preview=True
+)
+    # await m.delete()
+@bot.on_message(filters.regex("REQUEST/FEEDBACK"))
+async def bak(bot, msg):
+    await msg.reply(text="<b>Enter Your FeedBack OR Request, I Will Give It To My Boss</b>")
+        await asyncio.sleep(0.2)
         await msg.message.edit("○○○○○")
+        await asyncio.sleep(0.2)
         await msg.message.edit("●○○○○")
+        await asyncio.sleep(0.2)
         await msg.message.edit("●●○○○")
+        await asyncio.sleep(0.2)
         await msg.message.edit("●●●○○")
+        await asyncio.sleep(0.2)
         await msg.message.edit("●●●●○")
+        await asyncio.sleep(0.2)
         await msg.message.edit("●●●●●")
+        await asyncio.sleep(0.2)
         await msg.message.edit(
             text="Hᴇʀᴇ Is Yᴏᴜ'ʀᴇ Mᴇɴᴜ",
             reply_markup=InlineKeyboardMarkup(MENU_BUTTON)
